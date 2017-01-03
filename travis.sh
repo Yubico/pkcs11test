e3f2068630a684095982632a073e53ed99aa4d11
Thordur Bjornsson <thorduri@yubico.com>
2017-01-03 16:03:02 +0100

    test

--- travis.sh
+++ travis.sh
@@ -8,8 +8,8 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   git clone https://github.com/google/chaps-linux
   (
     cd chaps-linux || exit
-    make DPKGSIGN="-us -uc" package
-    dpkg -i ./*chaps*amd64.deb
+    CFLAGS="-Wno-error=char-subscripts" make DPKGSIGN="-us -uc" package
+    sudo dpkg -i ./*chaps*amd64.deb
   )
   make test_chaps
 fi
