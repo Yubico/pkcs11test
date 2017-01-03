9b88e759fef2275efaf6fe4f64bfbbb3e0126ac3
Thordur Bjornsson <thorduri@yubico.com>
2017-01-03 14:59:12 +0100

    travis: Test chaps.
    Lifted from github.com/google/pkcs11test@2342e7e

--- travis.sh
+++ travis.sh
@@ -3,3 +3,13 @@ set -evx
 env | sort
 
 make
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  git clone https://github.com/google/chaps-linux
+  (
+    cd chaps-linux || exit
+    make DPKGSIGN="-us -uc" package
+    dpkg -i ./*chaps*amd64.deb
+  )
+  make test_chaps
+fi
